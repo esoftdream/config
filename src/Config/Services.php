92435166a9b4113fcd3415f89a85425608b0b354
Toto <totoprayogo1916@gmail.com>
2025-08-19 22:02:39 +0700

    ♻️ refactor(config): perbarui service konfigurasi

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -5,13 +5,19 @@ namespace Esoftdream\Config;
 use CodeIgniter\Config\BaseService;
 use Esoftdream\Config;
 
+/**
+ * @method Config config(bool $getShared = true)
+ */
 class Services extends BaseService
 {
     /**
      * Returns the Settings manager class.
      */
-    public static function config()
+    public static function config($getShared = true)
     {
+        if ($getShared) {
+            return static::getSharedInstance('config');
+        }
 
         return new Config();
     }
